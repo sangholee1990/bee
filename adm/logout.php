2291e2701666674370f5fb99946d5ae989f58d6e
shlee1990 <sangho.lee.1990@gmail.com>
2023-04-23 21:38:05 +0900

    커밋

---
+++ adm/logout.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+	session_destroy();
+	header('Location: http://lab.phyzen.co.kr:8080/bees/20150917/adm/ma_login.html');
+?>
---
+++ data/cache/latest-beeinfo-basic-5-25.php
@@ -0,0 +1,83 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$bo_subject='꿀벌양봉지식';
+$list=array (
+  0 => 
+  array (
+    'wr_id' => '1',
+    'wr_num' => '-1',
+    'wr_reply' => '',
+    'wr_parent' => '1',
+    'wr_is_comment' => '0',
+    'wr_comment' => '0',
+    'wr_comment_reply' => '',
+    'ca_name' => '',
+    'wr_option' => '',
+    'wr_subject' => '꿀벌양봉지식 test 게시물 입니다.',
+    'wr_content' => '꿀벌양봉지식 test 게시물 입니다.꿀벌양봉지식 test 게시물 입니다.꿀벌양봉지식 test 게시물 입니다.꿀벌양봉지식 test 게시물 입니다.',
+    'wr_link1' => '',
+    'wr_link2' => '',
+    'wr_link1_hit' => '0',
+    'wr_link2_hit' => '0',
+    'wr_hit' => '29',
+    'wr_good' => '0',
+    'wr_nogood' => '0',
+    'mb_id' => 'admin',
+    'wr_password' => '*FD571203974BA9AFE270FE62151AE967ECA5E0AA',
+    'wr_name' => 'administrator',
+    'wr_email' => 'niceliberty@gmail.com',
+    'wr_homepage' => '',
+    'wr_datetime' => '2015-10-19 13:55:59',
+    'wr_file' => '0',
+    'wr_last' => '2015-10-19 13:55:59',
+    'wr_ip' => '192.168.0.1',
+    'wr_facebook_user' => '',
+    'wr_twitter_user' => '',
+    'wr_1' => '',
+    'wr_2' => '',
+    'wr_3' => '',
+    'wr_4' => '',
+    'wr_5' => '',
+    'wr_6' => '',
+    'wr_7' => '',
+    'wr_8' => '',
+    'wr_9' => '',
+    'wr_10' => '',
+    'is_notice' => false,
+    'subject' => '꿀벌양봉지식 test 게시물 입니다.',
+    'comment_cnt' => '',
+    'datetime' => '2015-10-19',
+    'datetime2' => '10-19',
+    'last' => '2015-10-19',
+    'last2' => '10-19',
+    'name' => '<span class="sv_member">administrator</span>',
+    'reply' => 0,
+    'icon_reply' => '',
+    'icon_link' => '',
+    'ca_name_href' => 'http://182.219.75.13/bbs/board.php?bo_table=beeinfo&amp;sca=',
+    'href' => 'http://182.219.75.13/bbs/board.php?bo_table=beeinfo&amp;wr_id=1',
+    'comment_href' => 'http://182.219.75.13/bbs/board.php?bo_table=beeinfo&amp;wr_id=1',
+    'icon_new' => '',
+    'icon_hot' => '',
+    'icon_secret' => '',
+    'link' => 
+    array (
+      1 => NULL,
+      2 => NULL,
+    ),
+    'link_href' => 
+    array (
+      1 => 'http://182.219.75.13/bbs/link.php?bo_table=beeinfo&amp;wr_id=1&amp;no=1',
+      2 => 'http://182.219.75.13/bbs/link.php?bo_table=beeinfo&amp;wr_id=1&amp;no=2',
+    ),
+    'link_hit' => 
+    array (
+      1 => 0,
+      2 => 0,
+    ),
+    'file' => 
+    array (
+      'count' => '0',
+    ),
+  ),
+)?>
